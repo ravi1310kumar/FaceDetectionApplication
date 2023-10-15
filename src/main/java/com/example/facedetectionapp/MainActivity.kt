0313c29ee88ac11aea1515c318d3d499b5a46cdc
Ravi Kumar <117887808+ravi1310kumar@users.noreply.github.com>
2023-10-15 16:01:34 +0530

    Add files via upload

---
+++ src/main/java/com/example/facedetectionapp/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.facedetectionapp
+
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.Toast
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btnface = findViewById<Button>(R.id.btnface)
+
+        btnface.setOnClickListener {
+
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            if(intent.resolveActivity(packageManager) !=null){
+                startActivityForResult(intent,123)
+            }
+            else{
+                 Toast.makeText(this, "oops something went wrong!!",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode==123&& resultCode == RESULT_OK){
+            val extras = data?.extras
+            val bitmap = extras?.get("data") as? Bitmap
+            if (bitmap != null) {
+                detectFace(bitmap)
+            }
+        }
+    }
+    private fun detectFace(bitmap: Bitmap){
+        val options = FaceDetectorOptions.Builder()
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+            .build()
+
+        val detector= FaceDetection.getClient(options)
+
+        val image = InputImage.fromBitmap(bitmap, 0)
+
+        val result = detector.process(image)
+            .addOnSuccessListener { faces ->
+                // Task completed successfully
+                var resultText = " "
+                var i=1
+                for(face in faces){
+                    resultText= "Face Number : $i" +
+                            "\nSmile : ${face.smilingProbability?.times(100)}%"+
+                            "\nLeft Eye Open : ${face.leftEyeOpenProbability?.times(100)}%"+
+                            "\nRight Eye Open : ${face.rightEyeOpenProbability?.times(100)}%"
+                    i++
+                }
+
+                if(faces.isEmpty()){
+                    Toast.makeText(this, "FACE NOT DETECTED PROPERLY!!",Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(this, resultText,Toast.LENGTH_LONG).show()
+                }
+            }
+            .addOnFailureListener { e ->
+                // Task failed with an exception
+                Toast.makeText(this, "oops something went wrong!!",Toast.LENGTH_LONG).show()
+            }
+    }
+}
